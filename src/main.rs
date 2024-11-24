f8e42e4112b87cd19556ff4a3094ab7d0b4edef0
Dhruvmehta18 <20.09.2004.bh@gmail.com>
2024-11-24 13:56:46 +0530

    changes for working with pm2 and binding to port

--- src/main.rs
+++ src/main.rs
@@ -41,8 +41,9 @@ async fn echo(
 }
 
 pub(crate) async fn handle_connection() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    // We create a TcpListener and bind it to 127.0.0.1:3000
-    let addr = SocketAddr::from(([127,0,0,1], 3000));
+    // We create a TcpListener and bind it to 127.0.0.1:$PORT
+    let port = get_env_variable("PORT", 7777);
+    let addr = SocketAddr::from(([127,0,0,1], port as u16));
     let listener = TcpListener::bind(addr).await?;
     info!("Listening on http://{}", addr);
 
